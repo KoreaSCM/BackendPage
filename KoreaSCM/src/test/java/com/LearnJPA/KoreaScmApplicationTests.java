e0029d1d9bccda26f0e9e1cc7f4bd5d41dc74d13
CASY82 <repaid11010@gmail.com>
2022-09-05 01:07:46 +0900

    first commit(Spring Boot)

---
+++ KoreaSCM/src/test/java/com/LearnJPA/KoreaScmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LearnJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoreaScmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
